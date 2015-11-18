513b79835c6f0584c376c43767aac3893a7fa963
Edgar Cobos <ecobos@edcobos.com>
2015-11-17 20:43:47 -0800

    slight modification to header file

--- laravel/resources/views/header.blade.php
+++ laravel/resources/views/header.blade.php
@@ -10,7 +10,7 @@
 	        <ul class="nav navbar-nav navbar-right">
 	            <li class="active"><a href="#"><b>Home</b></a></li>
 	            <li class="dropdown">
-	                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Options <b class="caret"></b></a>
+	                <a href="{{ url('/') }}" class="dropdown-toggle" data-toggle="dropdown">Options <b class="caret"></b></a>
 	                <ul class="dropdown-menu">
 	                    @if(Auth::check())
                             <li><a href="{{ url('/profile') }}"><b>Go to my account</b></a></li>
