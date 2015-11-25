6bc78b022834abb30803ee2d7faeab14fa939a60
Kelby Sapien <sapikelby@gmail.com>
2015-11-25 12:29:03 -0800

    Cancelling review works, redirects user to search page

---
+++ laravel/public/js/cancel_review.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('#alertMessage').hide();
+
+
+    $('#cancelReview').click(function () {
+        // redirects user back to search page
+        document.location = "/"
+    });
+
+});
